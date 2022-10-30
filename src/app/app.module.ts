f6bb0e401a265b7516ffd90ea4d9eef5da079191
Ashabenny123 <110253397+Ashabenny123@users.noreply.github.com>
2022-10-30 15:20:57 +0530

    pwa fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,9 @@ import { ScannerComponent } from './scanner/scanner.component';
 import { HistoryComponent } from './history/history.component';
 
 import { AngularFireModule } from '@angular/fire/compat';
+import { PwaService } from './services/pwa.service';
+const initializer = (pwaService: PwaService) => () =>
+  pwaService.initPwaPrompt();
 @NgModule({
   declarations: [AppComponent, ScannerComponent, HistoryComponent],
   imports: [
@@ -24,7 +27,15 @@ import { AngularFireModule } from '@angular/fire/compat';
       registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
-  providers: [],
+
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [PwaService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
